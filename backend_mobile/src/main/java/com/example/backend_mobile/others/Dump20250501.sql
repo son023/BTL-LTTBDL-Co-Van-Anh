a481f879f2f0a279a5221343be9bbadb63394c6b
son023 <sonnguyenvan122003@gmail.com>
2025-05-14 20:38:24 +0700

    fix data

--- backend_mobile/src/main/java/com/example/backend_mobile/others/Dump20250501.sql
+++ backend_mobile/src/main/java/com/example/backend_mobile/others/Dump20250501.sql
@@ -1717,27 +1717,27 @@ UPDATE phim
 SET trailer = 'https://www.youtube.com/embed/kVrqfYjkTdQ'
 WHERE id = 4;
 
-UPDATE phim
-SET trailer = 'https://www.youtube.com/embed/gCUg6Td5fgQ'
-WHERE id = 5;
+-- UPDATE phim
+-- SET trailer = 'https://www.youtube.com/embed/gCUg6Td5fgQ'
+-- WHERE id = 5;
 
 UPDATE phim
 SET trailer = 'https://www.youtube.com/embed/qtRKdVHc-cE'
-WHERE id = 6;
+WHERE id = 5;
 
 UPDATE phim
 SET trailer = 'https://www.youtube.com/embed/P5ieIbInFpg'
-WHERE id = 7;
+WHERE id = 6;
 
 UPDATE phim
 SET trailer = 'https://www.youtube.com/embed/zSWdZVtXT7E'
-WHERE id = 8;
+WHERE id = 7;
 
 UPDATE phim
 SET trailer = 'https://www.youtube.com/embed/WHXxVmeGQUc'
-WHERE id = 9;
+WHERE id = 8;
 
 UPDATE phim
 SET trailer = 'https://www.youtube.com/embed/n9xhJrPXop4'
-WHERE id = 10;
+WHERE id = 9;
 
